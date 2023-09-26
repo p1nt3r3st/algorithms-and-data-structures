f49f35b576c59ce26ffcd1652fcdf4f1f1503631
p1nt3r3st <evgenijbaharev04@gmail.com>
2023-09-26 18:29:45 +0300

    version 1

--- src/Main.java
+++ src/Main.java
@@ -8,53 +8,59 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 //массивы
-        System.out.print("Введите длину массива: ");
-        int len = in.nextInt(), sum = 0;
-        String ans = new String();
-
-        int numbers[] = new int[len];
-        for (int i = 0; i < len; i++){
-            System.out.printf("numbers[%d] = ", i); numbers[i] = in.nextInt();
-            sum += numbers[i];
-        }
-
-        System.out.println("Сумма массива: " + sum);
-        System.out.printf("Ср. арифметич.: %.5f", (double) sum / numbers.length);
+//        System.out.print("Введите длину массива: ");
+//        int len = in.nextInt(), sum = 0;
+//        String ans = new String();
+//
+//        int numbers[] = new int[len];
+//        for (int i = 0; i < len; i++){
+//            System.out.printf("numbers[%d] = ", i); numbers[i] = in.nextInt();
+//            sum += numbers[i];
+//        }
+//
+//        System.out.println("Сумма массива: " + sum);
+//        System.out.printf("Ср. арифметич.: %.5f", (double) sum / numbers.length);
 
 // Списки
 
-//        List<String> powers = new ArrayList<String>();
-//        System.out.println("Вводите элементы. Для прекращения введите `no.`");
-//        int i = 0;
-//        String my_line;
-//        do {
-//            System.out.printf("powers[%d] = ", i); my_line = in.nextLine();
-//            try {
-//                Integer.parseInt(my_line);
-//                powers.add(my_line);
-//                i += 1;
-//            }
-//            catch (NumberFormatException e){
-//                System.out.println("Введи либо число, либо `no`.");
-//            }
-//        } while (!my_line.contains("no"));
-//
-//
-//        int numbers[] = new int[powers.size()];
-//
-//        for (i = 0; i < powers.size(); i++){
-//            numbers[i] = Integer.parseInt(powers.get(i));
-//        }
-//        powers.clear();
-//
-//        System.out.println("Ваш массив:");
-//
-//        for (i = 0; i < numbers.length; i++){
-//            System.out.print(numbers[i] + " ");
-//        }
-//
-//        System.out.printf("\nМинимальный эл. = %d, максимальный эл. = %d", Arrays.stream(numbers).min().getAsInt(), Arrays.stream(numbers).max().getAsInt());
+        List<String> powers = new ArrayList<String>();
+        System.out.println("Вводите элементы. Для прекращения введите `no.`");
+        int i = 0;
+        String my_line;
+        do {
+            System.out.printf("powers[%d] = ", i); my_line = in.nextLine();
+            try {
+                Integer.parseInt(my_line);
+                powers.add(my_line);
+                i += 1;
+            }
+            catch (NumberFormatException e){
+                System.out.println("Введи либо число, либо `no`.");
+            }
+        } while (!my_line.contains("no"));
 
+        if (powers.size() != 0) {
+
+            int numbers[] = new int[powers.size()];
+
+            for (i = 0; i < powers.size(); i++) {
+                numbers[i] = Integer.parseInt(powers.get(i));
+            }
+            powers.clear();
+
+
+            System.out.println("Ваш массив:");
+
+
+            for (i = 0; i < numbers.length; i++) {
+                System.out.print(numbers[i] + " ");
+            }
+
+            System.out.printf("\nМинимальный эл. = %d, максимальный эл. = %d", Arrays.stream(numbers).min().getAsInt(), Arrays.stream(numbers).max().getAsInt());
+        }
+        else{
+            System.out.println("Ваш массив пуст :'(");
+        }
 
 // гармонический ряд 10
 //        int sum = 0;
